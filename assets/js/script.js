e58aecc17260b979358e9649c127814cdf6b48d7
Tejobhasa <tejobhasa.mm@gmail.com>
2024-05-26 01:00:38 +0630

    added body contents of html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Document loaded");
-    // Add any additional JavaScript functionality here
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+     event.preventDefault();
+
+        const name = document.getElementById('contact-name').value;
+        const message = document.getElementById('contact-message').value;
+
+        if (name && message) {
+            const contactData = {
+                 name: name,
+                 message: message
+                };
+
+            localStorage.setItem('contactData', JSON.stringify(contactData));
+            alert('Your message has been saved!');
+            } else {
+            alert('Please fill in all fields.');
+            }
 });
+
