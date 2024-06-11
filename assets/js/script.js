db946e3c1f75c29e4d4a63959f70b0d4b5678646
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-06-11 13:18:00 +0630

    modified link and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
+// order comfirm
 $(document).ready(function() {
     $(".order-btn").click(function() {
       var itemName = $(this).closest('.menu-item').find('h4').text();
       var itemPrice = $(this).closest('.menu-item').find('strong').text();
-      alert("Order placed! You ordered: " + itemName + " for " + itemPrice);
-      // Add further processing logic here, such as sending data to a server
+      alert("Confirm your order item. "+"You ordered : " + itemName + " for " + itemPrice + ".");
     });
   });
 
