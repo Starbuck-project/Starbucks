ca730efdb3b0acc1241a65bcd59389733527c018
agganyana <agganyana3@gmail.com>
2024-06-16 15:11:44 +0630

    modified

--- app.js
+++ app.js
@@ -1,89 +1,99 @@
+
+
 let openShopping = document.querySelector('.shopping');
 let closeShopping = document.querySelector('.closeShopping');
 let list = document.querySelector('.list');
 let listCard = document.querySelector('.listCard');
-let body = document.querySelector('.body');
+let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', ()=> {
+openShopping.addEventListener('click', () => {
+    body.classList.add('active');
+});
+
+closeShopping.addEventListener('click', () => {
     body.classList.remove('active');
-})
-closeShopping.addEventListener('click', ()=> {
-    body.classList.remove('active');
-})
+});
+
 let products = [
     {
         id: 1,
         name: 'PRODUCT NAME 1',
-        image: 'Bag1.jpeg',
-        price: 12000
+        image: 'bag.jpg',
+        price: 12
     },
     {
         id: 2,
         name: 'PRODUCT NAME 2',
-        image: 'snack2.jpeg',
-        price: 13000
+        image: 'T-shirt.jpeg',
+        price: 13
     },
     {
         id: 3,
         name: 'PRODUCT NAME 3',
-        image: '',
-        price: 22000
+        image: 'hat1.jpg',
+        price: 12
     },
     {
         id: 4,
         name: 'PRODUCT NAME 4',
-        image: '',
-        price: 125000
+        image: 'burger.avif',
+        price: 12
     },
     {
         id: 5,
         name: 'PRODUCT NAME 5',
-        image: '',
-        price: 150000
+        image: 'Bag1.jpeg',
+        price: 15
     },
     {
         id: 6,
         name: 'PRODUCT NAME 6',
-        image: '',
-        price: 160000
-    },   
+        image: 'snack2.jpeg',
+        price: 16
+    },
 ];
+
 let listCards = [];
-function initApp(){
-    products.forEach((value, key)=> {
+
+function initApp() {
+    products.forEach((value, key) => {
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
-        <img src"image/${value.image}"/>
+        <img src="./images1/${value.image}" alt="${value.name}" />
         <div class="title">${value.name}</div>
         <div class="price">${value.price.toLocaleString()}</div>
-        <button onclic="addToCard(${key})">Add To Card</button>
+        <button onclick="addToCard(${key})">Add To Card</button>
         `;
         list.appendChild(newDiv);
-    })
+    });
 }
+
 initApp();
-function addToCard(key){
-    if(listCards[key] == null){
-        listCards[key] = products[key];
-        listCards[key].quantity = 1;
+
+function addToCard(key) {
+    if (listCards[key] == null) {
+        listCards[key] = {...products[key], quantity: 1};
+    } else {
+        listCards[key].quantity += 1;
     }
     reloadCard();
 }
-function reloadCard(){
+
+function reloadCard() {
     listCard.innerHTML = '';
     let count = 0;
     let totalPrice = 0;
     listCards.forEach((value, key) => {
-        totalPrice = totalPrice + value.price;
-        count = count + value.quantity;
+        if (value != null) {
+            totalPrice += value.price * value.quantity;
+            count += value.quantity;
 
-        if(value != null){
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
-            <div><img src="image/${value.image}"/></div>
+            <div><img src="./images1/${value.image}" alt="${value.name}" /></div>
             <div>${value.name}</div>
             <div>${value.price.toLocaleString()}</div>
             <div>${value.quantity}</div>
@@ -95,16 +105,16 @@ function reloadCard(){
             `;
             listCard.appendChild(newDiv);
         }
-    })
+    });
     total.innerText = totalPrice.toLocaleString();
     quantity.innerText = count;
 }
+
 function changeQuantity(key, quantity) {
-    if (quantity == 0){
+    if (quantity == 0) {
         delete listCards[key];
-    }else {
+    } else {
         listCards[key].quantity = quantity;
-        listCards[key].price = quantitys[key].price;
     }
     reloadCard();
 }
